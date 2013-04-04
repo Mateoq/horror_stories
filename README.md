3e0cb109e022db7fea8674951dfee60cd5663641
Mateoq <quinterom1592@gmail.com>
2013-04-03 21:31:01 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Horror Stories Application
+
+This is a simple application, which contains awesome creepy pastas for my girlfriend.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HorrorStories::Application.config.secret_token = 'f2c36e47503a021ed32e61d17ce9162a0d0119bda5194882ad47c6ada9cbb6f1d6853936acfda5caccb78556c2ea1db95347eb1f5ec4cde07e07e390c3a0f2c8'
